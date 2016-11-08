2bbe62040e2a91ce28c7c35e8bda907f54e08edb
sasha <sasha@localhost.localdomain>
2016-11-08 19:50:57 +0300

    task2A

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/task2A', (req,res) => {
+   const sum = (parseInt(req.query.a,10)||0) + (parseInt(req.query.b)||0);
+   res.send(sum.toString());
+});
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
